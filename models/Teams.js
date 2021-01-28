0feb8ee0f57425f490c9780cd314843047ee2e47
Kemosabe2911 <stevinprince29@gmail.com>
2021-01-28 20:13:42 +0530

    Add team to db

---
+++ models/Teams.js
@@ -0,0 +1,22 @@
+const mongoose= require('mongoose');
+
+const TeamSchema= new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        },
+        score:{
+            type: Number,
+            required: true
+        },
+        date:{
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+const Team= mongoose.model('Team',TeamSchema);
+
+module.exports = Team;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,37 @@
 const express= require('express');
 const router= express.Router();
 
+//Team Model import
+const Team= require('../models/Teams');
+
 //Welcome Page
 router.get('/',(req,res) => res.render('index'));
+
+router.post('/',(req,res) =>{
+    const name= req.body.name;
+    const score= 0;
+    let errors=[];
+    if(!name){
+        errors.push({msg: "Please Enter a Team Name"});
+        console.log(errors);
+    }
+    else{
+        Team.findOne({name: name})
+        .then((team) =>{
+            if(team){
+                errors.push({msg: "Team name already registered"});
+                console.log(errors);
+            }
+            else{
+                const newTeam = new Team({name,score});
+                newTeam.save()
+                .then((team) =>{
+                    res.redirect('/');
+                    console.log("Success");
+                });
+            }
+        })
+    }
+})
 
 module.exports = router;
